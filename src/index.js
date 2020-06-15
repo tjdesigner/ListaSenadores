022c8e2d327f1ff270bc29237d3846f11b396b58
Tiago de Jesus <tiagodejesus.design@gmail.com>
2020-06-15 13:01:50 -0300

    Pagination - React-Bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-bootstrap-table-next/dist/react-bootstrap-table-next";
+import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator"
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,99 +1,55 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import api from "../../services/api";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+//import { Container, Row, Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import BootstrapTable from "react-bootstrap-table-next";
 
-export default class Main extends Component {
-state = { 
-  senadores: [],
-  codParlamentar:[],
+const Main = () => {
+
+  const [senadores, setSenadores] = useState([]);
+  const [codParlamentar, setCodParlamentar] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const getListData = async () => {
+    try {
+      const response = await api.get('/lista/atual.json')
+      setSenadores(response.data.ListaParlamentarEmExercicio.Parlamentares.Parlamentar.map(cod =>
+        cod=cod.IdentificacaoParlamentar));
+      setCodParlamentar(response.data.ListaParlamentarEmExercicio.Parlamentares.Parlamentar.map(cod =>
+        cod=cod.IdentificacaoParlamentar.CodigoParlamentar
+      ));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  console.log(senadores, codParlamentar);
+  
+
+  const columns = [
+    {dataField: "NomeParlamentar", text: "Nome do Parlamentar"},
+    {dataField: "CodigoParlamentar", text: "Código do Parlamentar"},
+    {dataField: "SiglaPartidoParlamentar", text: "Partido"},
+  ]
+  
+  useEffect(() => {
+    getListData();
+  }, [])
+
+  return (
+    <div>
+      {
+      senadores !== [''] ? (
+        <BootstrapTable
+        keyField="NomeParlamentar"
+        data={senadores}
+        columns={columns}
+        pagination={paginationFactory()}
+      />
+      ) : (
+        <h1>Tiago</h1>
+      )
+    }
+    </div>
+  )
 }
 
-  componentDidMount() {
-    this.loadSenadores();
-  }
-
-  loadSenadores = async () => {
-    const response = await api.get('/lista/atual.json')
-    this.setState({senadores: response.data.ListaParlamentarEmExercicio.Parlamentares.Parlamentar})
-    console.log(response.data.ListaParlamentarEmExercicio.Parlamentares);
-    this.setState({codParlamentar: response.data.ListaParlamentarEmExercicio.Parlamentares.Parlamentar.map(res =>{
-      this.codParlamentar = res.IdentificacaoParlamentar.CodigoParlamentar;
-      console.log(this.codParlamentar);
-        const response = api.get(`/${this.codParlamentar}/comissoes.json`);
-        response.then((value) => {
-          let cod = value.data.MembroComissaoParlamentar;
-          console.log(cod);
-        })
-    })})
-  }
-
-  render() {
-    return (
-      <>
-      <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Navbar.Collapse>
-      </Navbar>
-      <Container>
-        <p>listaSenadores: {this.state.senadores.length} </p>
-        <Row className="justify-content-md-center">
-          {this.state.senadores.map(senador =>(
-            <article key={senador.IdentificacaoParlamentar.CodigoParlamentar}>
-              <ul>
-                <li>
-                  <strong>
-                    Nome: {senador.IdentificacaoParlamentar.NomeCompletoParlamentar}
-                  </strong>
-                </li>
-                <li>
-                    UF: {senador.IdentificacaoParlamentar.UfParlamentar}
-                </li>
-                <li>
-                    Partido: {senador.IdentificacaoParlamentar.SiglaPartidoParlamentar}
-                </li>
-                <li>
-                  <img src={senador.IdentificacaoParlamentar.UrlFotoParlamentar}/>
-                </li>
-                <li>
-                  Membro da mesa: {senador.IdentificacaoParlamentar.MembroMesa}
-                </li>
-                <li>
-                  Membro da Liderança: {senador.IdentificacaoParlamentar.MembroLideranca}
-                </li>
-                <li>
-                  Página Parlamentar:
-                    <a href={senador.IdentificacaoParlamentar.UrlPaginaParlamentar}>
-                      {senador.IdentificacaoParlamentar.UrlPaginaParlamentar}
-                    </a>
-                </li>
-                <li>
-                  
-                </li>
-              </ul>
-
-            </article>
-          ))}
-        </Row>
-      </Container>
-      </>
-    )
-  }
-}
+export default  Main
